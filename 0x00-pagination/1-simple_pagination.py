c2aa9a42c836628f8e27b20377db62d0918dcefb
Nickson K Rop <nickyrutto@gmail.com>
2024-06-22 00:53:34 -0700

    simple pagination of a csv file

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+'''
+This module implements a simple helper function
+'''
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''
+    This function calculates the start and end indexes for
+    a given page and size
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page
+
+    Returns:
+        tuple: A tuple containing the start index and end index.
+    '''
+    start_idx = (page - 1) * page_size
+    end_idx = page * page_size
+    return (start_idx, end_idx)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        This function gets a page with the specified number of items
+        ARGS:
+            page (int) - page number
+            page_size (int) - number of items per page
+        '''
+        assert isinstance(page, int) and page > 0, "page must be a
+        positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size
+        must be a positive integer"
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        if start >= len(dataset):
+            return []
+
+        return dataset[start:end]
