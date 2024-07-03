cd4f20d80b20f377897a509033da3d4e51405ee7
Nickson K Rop <nickyrutto@gmail.com>
2024-07-03 06:40:32 -0700

    parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''
+Setting up a basic Flask app
+'''
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+
+
+class Config:
+    '''
+    This class configures available languages, default local e and timezone
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def home():
+    '''This function renders an HTML template'''
+    title = _("home_title")
+    header = _("home_header")
+    return render_template('3-index.html', title=title, header=header)
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    This function gets a locale from request
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
